3c66603d3a1952776cd4caea2bed319447f9c0d7
MSNkosi31 <142292604+MSNkosi31@users.noreply.github.com>
2025-07-05 15:42:29 +0200

    First Four Cell Arduino Sim
    Started an Arduinos simulation with 4 cells that currently blinks the first cell to test

--- ArduinoSim/Braille display/src/main.cpp
+++ ArduinoSim/Braille display/src/main.cpp
@@ -2,17 +2,60 @@
 
 // put function declarations here:
 int myFunction(int, int);
+//cell 1
+int l11 = 2;
+int l12 = 3;
+int l13 = 4;
+int l14 = 5;
+int l15 = 6;
+int l16 = 7;
+//cell 2
+//int l21 = 4;
+//int l22 = 4;
+//int l23 = 4;
+//int l24 = 4;
+//int l25 = 4;
+//int l26 = 4;
 
+void Write() {
+  //cell 1
+  digitalWrite(l11, 1);  
+  digitalWrite(l12, 1);
+  digitalWrite(l13, 1);  
+  digitalWrite(l14, 1);
+  digitalWrite(l15, 1);  
+  digitalWrite(l16, 1);
+}
+
+void Clear(){
+  digitalWrite(l11, 0);  
+  digitalWrite(l12, 0);
+  digitalWrite(l13, 0);  
+  digitalWrite(l14, 0);
+  digitalWrite(l15, 0);  
+  digitalWrite(l16, 0);
+}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//cell 1
+pinMode(l11, OUTPUT);
+pinMode(l12, OUTPUT);
+pinMode(l13, OUTPUT);
+pinMode(l14, OUTPUT);
+pinMode(l15, OUTPUT);
+pinMode(l16, OUTPUT);
+//cell 2
+//pinMode(l21, OUTPUT);
+//pinMode(l22, OUTPUT);
+//pinMode(l23, OUTPUT);
+//pinMode(l24, OUTPUT);
+//pinMode(l25, OUTPUT);
+//pinMode(l26, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // put your main code here, to run repeatedly:  
+  Write();
+  delay(2000);
+  Clear();
+  delay(2000);
 }
