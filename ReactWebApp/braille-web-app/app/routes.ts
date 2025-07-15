9975bdce15f7c0491bb0fa8508bad1e3272f3119
Thapelo <thapelo.mphahlele@hisgroup-it.co.za>
2025-07-15 12:07:10 +0200

    created signin and signup pages

---
+++ ReactWebApp/braille-web-app/app/routes.ts
@@ -0,0 +1,6 @@
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
+
+export default [
+    index("routes/signin.tsx"),
+    route("signup", "routes/signup.tsx")
+] satisfies RouteConfig;
