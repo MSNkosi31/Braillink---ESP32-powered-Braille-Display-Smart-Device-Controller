63e9b6f2b96eb6792102412c0ec10aad503bf849
MSNkosi31 <142292604+MSNkosi31@users.noreply.github.com>
2025-07-21 13:08:06 +0200

    Dynamic braille display

---
+++ ArduinoSim/ESP32 Braille Display/.pio/build/featheresp32-s2/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/featheresp32-s2/FrameworkArduino/libb64/cencode.c.o: \
+ C:/Users/andil/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ C:/Users/andil/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
