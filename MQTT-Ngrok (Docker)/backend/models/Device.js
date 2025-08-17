88262e0b47b39d23962733f56b2c027b05b38e1b
MSNkosi31 <142292604+MSNkosi31@users.noreply.github.com>
2025-08-17 22:05:27 +0200

    Revert "Merge branch 'AndileTest'"
    This reverts commit 7fcb3fb29a77e9896036d3a24f8daa268b4ed14b, reversing
    changes made to 377cff2fea40db4744b22b3156290c28f159f371.

--- MQTT-Ngrok (Docker)/backend/models/Device.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DeviceSchema = new mongoose.Schema({
-  device: String,
-  subscribeTopic: String,
-  publishTopic: String
-});
-
-module.exports = mongoose.model("Device", DeviceSchema);
