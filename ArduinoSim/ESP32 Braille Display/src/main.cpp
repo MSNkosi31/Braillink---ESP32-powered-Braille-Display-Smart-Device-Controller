b3316d47b6232b4972f2ef94ccb7c8cf4c19458b
Ludwig Booysen <77204063+LudwigBooysen@users.noreply.github.com>
2025-08-26 15:23:25 +0200

    Update
    Bug fixes:
    - Fixed LCD screen flickkering
    - Did so code cleaning and commeting cleaning
    - Removed unneeded code like function declartions etc.
    - Main menu and sub menus now use the same array ensuring more storage is open on the ESP32 Device
    
    Features:
    - Menu added
    - Working submenu
    - Made it possible to add and remove rooms and devices to the menu
    - Removed next and previous buttons
    - Added rotary dial
    - Optimized the way the devices goes into sleep mode and processes sleep mode entry and exits.
    - Future planning for recieving and acting out actions
    - Future planning for loading menu options from a database

--- ArduinoSim/ESP32 Braille Display/src/main.cpp
+++ ArduinoSim/ESP32 Braille Display/src/main.cpp
@@ -1,4 +1,4 @@
-#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
 #include <SPI.h>
 #include <WiFi.h>
 #include <map>
@@ -17,7 +17,7 @@ const char* password = "";
 #define DATA_PIN 23
 #define CS_PIN 21
 
-MD_MAX72XX matrix = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+MD_MAX72XX matrix = MD_MAX72XX(MD_MAX72XX::PAROLA_HW, CS_PIN, MAX_DEVICES);
 
 //=================== LCD SCREEN===================
 LiquidCrystal_I2C lcd(0x27, 16, 2);  
@@ -28,7 +28,9 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 #define NEXT_PIN 14
 #define PREV_PIN 27
 
-// Test
+// =================== ROTARY DIAL ===============
+#define ENCODER_CLK 2
+#define ENCODER_DT  4
 
 // =================== BUZZER ===================
 #define BUZZ_PIN 22
@@ -77,22 +79,22 @@ std::map<char,String> letters = {
 };
 // =================== GLOBAL VARIABLES ===================
 
-String binLetter [13] = {"000000","000000","000000","000000","000000","000000","000000","000000","000000","000000","000000","000000","000000"};//Sets all leds off
-int menuOption = 0;
+String binLetter[13] = {"000000","000000","000000","000000","000000","000000","000000","000000","000000","000000","000000","000000","000000"};//Sets all leds off
+int currentMainOption = 0;
+int currentSubOption = 0;
 bool welcomeShown = false;
 bool wakeup = false;
 bool AllWake = false;
 unsigned long lastActiveTime = 0;
-const unsigned long activeDuration = 10000; // 60 seconds and yes
-
-// =================== FUNCTION DECLARATIONS ===================
-void clearBin();
-void convertWord(String word, String binLetter[]);
-void displayBrailleFormat(String c1, String c2, String c3, String c4, String c5, String c6,
-                          String c7, String c8, String c9, String c10, String c11, String c12, String c13);
-void showWelcomeMessage();
-void playBuzz(int freq = 1000, int duration = 200);
-
+const unsigned long activeDuration = 10000; // 60 seconds
+String mainMenu[] = {"bedroom-lights1,lights2", "bathroom-gyser", "kitchen-thermostats"};
+bool shownGoodbyeMsg = false;
+bool inSubMenu = false;
+const int MAX_ROOMS = 10;
+String newSubMenu[MAX_ROOMS];  // MAX amount of devices
+int subMenuCount = 0;   // how many devices are in newSubMenu
+int mainMenuCount = sizeof(mainMenu) / sizeof(mainMenu[0]); // safe main menu count
+int lastClk = HIGH;
 
 void setup() {
   Serial.begin(9600);
@@ -104,8 +106,13 @@ void setup() {
   pinMode(PREV_PIN, INPUT_PULLDOWN);
   pinMode(ENTER_PIN, INPUT_PULLDOWN);
   pinMode(BACK_PIN, INPUT_PULLDOWN);
+
   pinMode(BUZZ_PIN, OUTPUT);
+
   pinMode(PROX_PIN, INPUT);
+
+  pinMode(ENCODER_CLK, INPUT);
+  pinMode(ENCODER_DT, INPUT);
 
   Serial.print("Connecting to ");
   Serial.println(ssid);
@@ -134,26 +141,36 @@ void setup() {
   matrix.update();
 }
 
-
-
-
 void loop() {
- // just safety check
+  // just safety check
   if (!welcomeShown) return; 
-  
-  String main[] = {"lights", "doorlocks", "theromstat"};
-  String lights[] = {"bedroom#1", "bedroom#2", "kitchen"};
  
+  // Rotary dial states
+  bool nextTruned = false;
+  bool prevTurned = false;
+
+  // Read Rotary Input
+  int newClk = digitalRead(ENCODER_CLK);
+  if (newClk != lastClk) {
+    // There was a change on the CLK pin
+    lastClk = newClk;
+    int dtValue = digitalRead(ENCODER_DT);
+    if (newClk == LOW && dtValue == HIGH) {
+      nextTruned = true;
+    }
+    if (newClk == LOW && dtValue == LOW) {
+      prevTurned = true;
+    }
+  }
+
   //Read the state of the different pins
-  bool nextPressed  = digitalRead(NEXT_PIN) == HIGH;
-  bool prevPressed  = digitalRead(PREV_PIN) == HIGH;
   bool enterPressed = digitalRead(ENTER_PIN) == HIGH;
   bool backPressed  = digitalRead(BACK_PIN) == HIGH;
   bool motionDetected = digitalRead(PROX_PIN) == HIGH;
  
-   bool activityAll = motionDetected || nextPressed || prevPressed || enterPressed || backPressed;
-   bool activityButtons =  nextPressed || prevPressed || enterPressed || backPressed;
-   bool isActive = (millis() - lastActiveTime) < activeDuration;
+  bool activityAll = motionDetected || nextTruned || prevTurned || enterPressed || backPressed;
+  bool activityButtons = nextTruned || prevTurned || enterPressed || backPressed;
+  bool isActive = (millis() - lastActiveTime) < activeDuration;
    
   if (!isActive && activityAll) {
     wakeup = true;
@@ -165,12 +182,10 @@ void loop() {
     if (AllWake) {
       showWelcomeMessage();
       delay(2000);
-      matrix.clear();
-      matrix.update();
-   
+      updateMenu(0, mainMenu, mainMenuCount);
     }
-     lastActiveTime = millis();
-       wakeup = false;
+    lastActiveTime = millis();
+    wakeup = false;
     AllWake = false;
     return;
   }
@@ -180,110 +195,106 @@ void loop() {
     if (activityAll) {
       lastActiveTime = millis(); // keep alive
     }
-    Serial.println("Display ON (motion/button active)");
-
-    clearBin();
-    convertWord(lights[menuOption], binLetter);
-    matrix.update();
-
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Selected:");
-    lcd.setCursor(0, 1);
-    lcd.print(lights[menuOption]);
+    //Serial.println("Display ON (motion/button active)");
     // Handle buttons only if pressed
-    if (nextPressed) {
-      digitalWrite(BUZZ_PIN, HIGH);
-      delay(1000); // debounce
-      digitalWrite(BUZZ_PIN, LOW);
-      menuOption++;
-      if (menuOption > 2) menuOption = 0;
+
+    if (nextTruned) {
+      nextTruned = false;
+      playBuzz(831, 100); // 831 = G#5 pitch
+      if (inSubMenu) {
+        currentSubOption++;
+        updateMenu(currentSubOption, newSubMenu, subMenuCount);
+      } else {
+        currentMainOption++;
+        updateMenu(currentMainOption, mainMenu, mainMenuCount);
+      }
     }
 
-    if (prevPressed) {
-      digitalWrite(BUZZ_PIN, HIGH);
-      delay(500); // debounce
-      digitalWrite(BUZZ_PIN, LOW);
-      delay(500); // extra debounce
-      menuOption--;
-      if (menuOption < 0) menuOption = 2;
+    if (prevTurned) {
+      prevTurned = false;
+      playBuzz(659, 100); // 659 = E5 pitch
+      if (inSubMenu) {
+        currentSubOption++;
+        updateMenu(currentSubOption, newSubMenu, subMenuCount);
+      } else {
+        currentMainOption--;
+        updateMenu(currentMainOption, mainMenu, mainMenuCount);
+      }
     }
+
+    if (enterPressed) {
+      if (!inSubMenu) {
+        playBuzz(523, 100); // 523 = C5 pitch
+        inSubMenu = true;
+        currentSubOption = 0;
+        getDevices(currentMainOption);
+        updateMenu(currentSubOption, newSubMenu, subMenuCount);
+      } else if (inSubMenu) { 
+        // Do action / send messages
+      }
+    }
+
+    if (backPressed && inSubMenu) {
+      playBuzz(523, 100); // 523 = C5 pitch
+      inSubMenu = false;
+      currentSubOption = 0;
+      updateMenu(currentMainOption, mainMenu, mainMenuCount);
+    }
+
   } 
   else {
     
-    Serial.println("Display OFF (no motion/button for 60 seconds)");
-    matrix.clear();
-    matrix.update();
+    if (!shownGoodbyeMsg) {
+      //Serial.println("Display OFF (no motion/button for 60 seconds)");
+      matrix.clear();
+      matrix.update();
     
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Goodbye");
-    lcd.setCursor(0,1);
-    lcd.print("User");
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Goodbye");
+      lcd.setCursor(0,1);
+      lcd.print("User");
+
+      shownGoodbyeMsg = true;
+    }
+
   }
 
-  // read the state of the switch/button:
-  String word[15] = {"bathroom","kitchen","bedroom","lights", "doorlocks", "thermostats", "locked", "unlocked"} ;
 
-  
-  
-  //convertWord(word[r], binLetter);
-
-  /*if(digitalRead(NEXT_PIN) == HIGH){
-    Serial.println("The state changed HIGH");
-    //digitalWrite(BLUE_LED, true);
-    r++;}*/
-
-  //if(digitalRead(PREV_PIN) == HIGH){
-    //Serial.println("The state changed HIGH");
-    //digitalWrite(YELLOW_LED, true);
-    //r--;}
-
-  //if(digitalRead(ENTER_PIN) == LOW){
-    //Serial.println("The state changed LOW ");
-    //digitalWrite(GREEN_LED, true);
-    //}
-
-  //delay(10000);
-  
-  /*String word[15] = {"bathroom","kitchen","bedroom","lights", "doorlocks", "thermostats", "locked", "unlocked"} ;
-  //convertWord(word, binLetter);
-  //delay(100000);
-  int i = 0;
-
-  while(i < sizeof(word)){
-    clearBin();
-    convertWord(word[i], binLetter);
-    i++;
-    delay(3000);
-  }*/
- 
-
-  /*switch (menuOption) {
-    case 0:
-      convertWord(word[menuOption], binLetter);
-      Serial.println(word[menuOption]);
-      break;
-
-    case 1:
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.println("Menu 1: LED ON");
-      break;
-
-    case 2:
-      Serial.println("Menu 2: Blinking LED");
-      digitalWrite(LED_BUILTIN, HIGH);
-      delay(250);
-      digitalWrite(LED_BUILTIN, LOW);
-      delay(250);
-      break;
-  }*/
-delay(50);
-  
-//clearBin(); // Slow down loop to see messages
 }
 
 // =================== FUNCTIONS ===================
+
+void updateMenu(int idx, String selectedMenu[], int menuCount) {
+  // wrap index
+  if (idx >= menuCount) idx = 0;
+  if (idx < 0) idx = menuCount - 1;
+
+  if (inSubMenu) {
+    currentSubOption = idx;
+  } else {
+    currentMainOption = idx;
+  }
+
+  matrix.clear();
+  clearBin();
+  convertWord(selectedMenu[idx], binLetter);
+  matrix.update();
+
+  String lcdDisplayWord = selectedMenu[idx];
+  if (lcdDisplayWord.indexOf('-') != -1) {
+    lcdDisplayWord = lcdDisplayWord.substring(0, lcdDisplayWord.indexOf('-'));
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Selected:");
+  lcd.setCursor(0, 1);
+  lcd.print(lcdDisplayWord);
+}
+
+
+
 void playBuzz(int freq , int duration) {
   tone(BUZZ_PIN, freq);     // Play tone at 'freq' Hz
   delay(duration);          // Wait for 'duration' ms
@@ -291,7 +302,7 @@ void playBuzz(int freq , int duration) {
 }
 
 void clearBin(){
-  for (int i=0; i < 13; i++){// 13 is too hardcoded 
+  for (int i = 0; i < 13; i++){// 13 is too hardcoded 
     binLetter[i] = "000000";
   }
 }
@@ -310,158 +321,80 @@ void showWelcomeMessage() {
 }
 
 void convertWord(String word, String binLetter[]){//this method converts words into a binary value used to turn certain leds on or off
-  
-  Serial.println(word);
+
+  //Serial.println(word);
   for(int i = 0; i < word.length(); i++){
+    // Validation
+    if (word[i] == '-') break;
+    if (word[i] == ' ') continue;
+
     binLetter[i] = letters[word[i]].c_str(); //convert this specific letter to the set binary in the map
   }
 
-  String cell1 = binLetter[0];
-  //Serial.println(cell1);// for testing
-  String cell2 = binLetter[1];
-  //Serial.println(cell2);
-  String cell3 = binLetter[2];
-  //Serial.println(cell3);
-  String cell4 = binLetter[3];
-  //Serial.println(cell4);
-  String cell5 = binLetter[4];
-  //Serial.println(cell5);
-  String cell6 = binLetter[5];
-  //Serial.println(cell6);
-  String cell7 = binLetter[6];
-  //Serial.println(cell7);
-  String cell8 = binLetter[7];
-  //Serial.println(cell8);
-  String cell9 = binLetter[8];
-  //Serial.println(cell9);
-  String cell10 = binLetter[9];
-  //Serial.println(cell10);
-  String cell11 = binLetter[10];
-  //Serial.println(cell11);
-  String cell12 = binLetter[11];
-  //Serial.println(cell12);
-  String cell13 = binLetter[12];
-  //Serial.println(cell13);
-  displayBrailleFormat(cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8, cell9, cell10, cell11, cell12, cell13);   
+  displayBrailleFormat(binLetter, word);
+
 }
-void displayBrailleFormat(String cell1, String cell2, String cell3, String cell4, String cell5, String cell6, String cell7, String cell8, String cell9, String cell10, String cell11, String cell12, String cell13) {
-//this sucked to hell, DON'T TOUCH!!!! 
-  //CELL1
-    matrix.setPoint(1, 57, cell1[0]- '0');
-    matrix.setPoint(3, 57, cell1[1]- '0');
-    matrix.setPoint(5, 57, cell1[2]- '0');
 
-    matrix.setPoint(1, 59, cell1[3]- '0');
-    matrix.setPoint(3, 59, cell1[4]- '0');
-    matrix.setPoint(5, 59, cell1[5]- '0');
+void displayBrailleFormat(String cells[], String menuOption) {
+  
+  //CELLS
+  int possibleCells = MAX_DEVICES * 2;
+  int cellIdx = 0;
+  int maxChars = menuOption.length();
+  for (int i = possibleCells; i > 0; i--) {
+    matrix.setPoint(1, (i*possibleCells/4)-2-(possibleCells-i), cells[cellIdx][0] - '0');
+    matrix.setPoint(3, (i*possibleCells/4)-2-(possibleCells-i), cells[cellIdx][1] - '0');
+    matrix.setPoint(5, (i*possibleCells/4)-2-(possibleCells-i), cells[cellIdx][2] - '0');
 
-    //CELL2
-    matrix.setPoint(1, 62, cell2[0]- '0');
-    matrix.setPoint(3, 62, cell2[1]- '0');
-    matrix.setPoint(5, 62, cell2[2]- '0');
+    matrix.setPoint(1, (i*possibleCells/4)-4-(possibleCells-i), cells[cellIdx][3] - '0');
+    matrix.setPoint(3, (i*possibleCells/4)-4-(possibleCells-i), cells[cellIdx][4] - '0');
+    matrix.setPoint(5, (i*possibleCells/4)-4-(possibleCells-i), cells[cellIdx][5] - '0');
 
-    matrix.setPoint(1, 48, cell2[3]- '0');
-    matrix.setPoint(3, 48, cell2[4]- '0');
-    matrix.setPoint(5, 48, cell2[5]- '0');
+    cellIdx++;
 
-    //CELL3
-    matrix.setPoint(1, 51, cell3[0]- '0');
-    matrix.setPoint(3, 51, cell3[1]- '0');
-    matrix.setPoint(5, 51, cell3[2]- '0');
+    if (cellIdx > maxChars) {
+      break;
+    }
+  }
 
-    matrix.setPoint(1, 53, cell3[3]- '0');
-    matrix.setPoint(3, 53, cell3[4]- '0');
-    matrix.setPoint(5, 53, cell3[5]- '0');
+}
 
-    //CELL4
-    matrix.setPoint(1, 40, cell4[0]- '0');
-    matrix.setPoint(3, 40, cell4[1]- '0');
-    matrix.setPoint(5, 40, cell4[2]- '0');
+int countCharOccurrences(const String& inputString, char targetChar) {
+  int count = 0;
+  for (int i = 0; i < inputString.length(); i++) {
+    if (inputString.charAt(i) == targetChar) {
+      count++;
+    }
+  }
+  return count;
+}
 
-    matrix.setPoint(1, 42, cell4[3]- '0');
-    matrix.setPoint(3, 42, cell4[4]- '0');
-    matrix.setPoint(5, 42, cell4[5]- '0');
+void getDevices(int idxMain) {
+  String menuName = mainMenu[idxMain];
 
-    //CELL5
-    matrix.setPoint(1, 45, cell5[0]- '0');
-    matrix.setPoint(3, 45, cell5[1]- '0');
-    matrix.setPoint(5, 45, cell5[2]- '0');
+  if (menuName.indexOf('-') == -1) {
+    subMenuCount = 0;
+    return;
+  }
 
-    matrix.setPoint(1, 47, cell5[3]- '0');
-    matrix.setPoint(3, 47, cell5[4]- '0');
-    matrix.setPoint(5, 47, cell5[5]- '0');
+  int looptimes = countCharOccurrences(menuName, ',') + 1;
+  int idxD1 = menuName.indexOf('-') + 1;
+  int idxD2;
 
+  for (int i = 0; i < looptimes; i++) {
+    idxD2 = menuName.indexOf(',', idxD1);
+    if (idxD2 == -1) {
+      idxD2 = menuName.length();
+    }
 
-    //CELL6
-    matrix.setPoint(1, 34, cell6[0]- '0');
-    matrix.setPoint(3, 34, cell6[1]- '0');
-    matrix.setPoint(5, 34, cell6[2]- '0');
+    newSubMenu[i] = menuName.substring(idxD1, idxD2);
+    idxD1 = idxD2 + 1;
+  }
 
-    matrix.setPoint(1, 36, cell6[3]- '0');
-    matrix.setPoint(3, 36, cell6[4]- '0');
-    matrix.setPoint(5, 36, cell6[5]- '0');
+  subMenuCount = looptimes;
 
-
-    //CELL7
-    matrix.setPoint(1, 39, cell7[0]- '0');
-    matrix.setPoint(3, 39, cell7[1]- '0');
-    matrix.setPoint(5, 39, cell7[2]- '0');
-
-    matrix.setPoint(1, 25, cell7[3]- '0');
-    matrix.setPoint(3, 25, cell7[4]- '0');
-    matrix.setPoint(5, 25, cell7[5]- '0');
-
-    //CELL8
-    matrix.setPoint(1, 28, cell8[0]- '0');
-    matrix.setPoint(3, 28, cell8[1]- '0');
-    matrix.setPoint(5, 28, cell8[2]- '0');
-
-    matrix.setPoint(1, 30, cell8[3]- '0');
-    matrix.setPoint(3, 30, cell8[4]- '0');
-    matrix.setPoint(5, 30, cell8[5]- '0');
-
-    //CELL9
-    matrix.setPoint(1, 17, cell9[0]- '0');
-    matrix.setPoint(3, 17, cell9[1]- '0');
-    matrix.setPoint(5, 17, cell9[2]- '0');
-
-    matrix.setPoint(1, 19, cell9[3]- '0');
-    matrix.setPoint(3, 19, cell9[4]- '0');
-    matrix.setPoint(5, 19, cell9[5]- '0');
-
-    //CELL10
-    matrix.setPoint(1, 22, cell10[0]- '0');
-    matrix.setPoint(3, 22, cell10[1]- '0');
-    matrix.setPoint(5, 22, cell10[2]- '0');
-
-    matrix.setPoint(1, 8, cell10[3]- '0');
-    matrix.setPoint(3, 8, cell10[4]- '0');
-    matrix.setPoint(5, 8, cell10[5]- '0');
-
-    //CELL11
-    matrix.setPoint(1, 11, cell11[0]- '0');
-    matrix.setPoint(3, 11, cell11[1]- '0');
-    matrix.setPoint(5, 11, cell11[2]- '0');
-
-    matrix.setPoint(1, 13, cell11[3]- '0');
-    matrix.setPoint(3, 13, cell11[4]- '0');
-    matrix.setPoint(5, 13, cell11[5]- '0');
-
-    //CELL12
-    matrix.setPoint(1, 0, cell12[0]- '0');
-    matrix.setPoint(3, 0, cell12[1]- '0');
-    matrix.setPoint(5, 0, cell12[2]- '0');
-
-    matrix.setPoint(1, 2, cell12[3]- '0');
-    matrix.setPoint(3, 2, cell12[4]- '0');
-    matrix.setPoint(5, 2, cell12[5]- '0');
-
-    //CELL12
-    matrix.setPoint(1, 5, cell12[0]- '0');
-    matrix.setPoint(3, 5, cell12[1]- '0');
-    matrix.setPoint(5, 5, cell12[2]- '0');
-
-    matrix.setPoint(1, 7, cell12[3]- '0');
-    matrix.setPoint(3, 7, cell12[4]- '0');
-    matrix.setPoint(5, 7, cell12[5]- '0');
+  // clear leftovers
+  for (int i = looptimes; i < 10; i++) {
+    newSubMenu[i] = "";
+  }
 }
