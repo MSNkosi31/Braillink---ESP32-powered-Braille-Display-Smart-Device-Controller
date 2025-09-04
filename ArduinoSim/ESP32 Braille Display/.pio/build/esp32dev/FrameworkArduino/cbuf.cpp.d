b2a56bcb2cbd917427471da7e0babdef6ebb640b
Gift <direodirile95@gmail.com>
2025-09-04 09:00:13 +0200

    update on connecting to the broker over TCP
    log showing =[MQTT] connecting to 4.tcp.eu.ngrok.io:12944
    but the terminal is not revealing if we indeed connected

--- ArduinoSim/ESP32 Braille Display/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++ ArduinoSim/ESP32 Braille Display/.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -1,3 +1,3 @@
 .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/ettie/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/ettie/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
+ C:/Users/direo/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/direo/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- ArduinoSim/ESP32 Braille Display/.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.d
+++ ArduinoSim/ESP32 Braille Display/.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.d
@@ -1,3 +1,3 @@
 .pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.o: \
- C:/Users/ettie/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- C:/Users/ettie/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
+ C:/Users/direo/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
+ C:/Users/direo/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
